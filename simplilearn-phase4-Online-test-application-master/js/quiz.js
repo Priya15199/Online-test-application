3493d385ba40db7ed97cb4333039011586546b6c
Priya15199 <101008490+Priya15199@users.noreply.github.com>
2022-10-09 17:56:31 +0530

    Add files via upload

---
+++ simplilearn-phase4-Online-test-application-master/js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"What is the name of the ancient citadel in Athens, Greece?",
+        option:[
+            "1.Athens",
+            "2.Thessaloniki",
+            "3.Acropolis",
+            "4.Serres",
+        ],
+        answer:3,
+    },
+    {
+        question:"Where is Neuschwanstein Castle?",
+        option:[
+            "1.UK",
+            "2.Germany",
+            "3.Belgium",
+            "4.Italy",
+            
+        ],
+        answer:2,
+    },
+    {
+        question:"Which is the tallest waterfall in the world?",
+        option:[
+            "1.Victoria Falls (Zimbabwe)",
+            "2.Niagara Falls (Canada)",
+            "3.Angel Falls (Venezuela)",
+           "4.Iguazu Falls (Argentina and Brazil)",   
+        ],
+        answer:3,
+    },
+    {
+        question:"What is the name of the U.K. palace considered to be the full-time home of the Queen?",
+        option:[
+            "1.Kensington Palace",
+            "2.Buckingham Palace",
+            "3.Blenheim Palace",
+            "4.Windsor Castle",   
+        ],
+        answer:2,
+
+    },
+    {
+        question:"Which city is Angkor Wat situated in?",
+        option:[
+            "1.Phnom Penh",
+            "2.Kampong Cham",
+            "3.Sihanoukville",
+            "4.Siem Reap",   
+        ],
+        answer:4,
+    }
+]
